89b829a1ea8474dfc51e079ae7fc02bb5993fd90
Richard Garcia <code.richardy@gmail.com>
2025-08-24 15:55:04 -0300

    this commit introduces dedicated classes for authentication and identity errors, providing a centralized and structured way to manage and reuse error definitions throughout the sdk.

---
+++ Source/Errors/IdentityErrors.cs
@@ -0,0 +1,9 @@
+namespace Vinder.IdentityProvider.Sdk.Errors;
+
+public static class IdentityErrors
+{
+    public static readonly Error UserAlreadyExists = new(
+        Code: "#VINDER-IDP-ERR-IDN-409",
+        Description: "The user with the specified username already exists. See https://bit.ly/errors-reference for more details."
+    );
+}
