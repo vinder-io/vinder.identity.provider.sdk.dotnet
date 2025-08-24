be18c029bc513a5165f668ac37ba4bff924dcb13
Richard Garcia <code.richardy@gmail.com>
2025-08-24 16:33:40 -0300

    this commit introduces a default grant type for client authentication credentials

--- Source/Payloads/Identity/ClientAuthenticationCredentials.cs
+++ Source/Payloads/Identity/ClientAuthenticationCredentials.cs
@@ -2,7 +2,7 @@ namespace Vinder.IdentityProvider.Sdk.Payloads.Identity;
 
 public sealed record ClientAuthenticationCredentials
 {
-    public string GrantType { get; init; } = default!;
+    public string GrantType { get; init; } = "client_credentials";
     public string ClientId { get; init; } = default!;
     public string ClientSecret { get; init; } = default!;
 }
