db60b5e51ba60515c70b6219f408fe913a71b4c9
Richard Garcia <code.richardy@gmail.com>
2025-08-26 20:42:47 -0300

    this commit introduces a dedicated tenanterrors class to centralize and standardize tenant-related error handling.

---
+++ Source/Errors/TenantErrors.cs
@@ -0,0 +1,24 @@
+namespace Vinder.IdentityProvider.Sdk.Errors;
+
+public static class TenantErrors
+{
+    public static readonly Error HttpContextUnavailable = new(
+        Code: "#VINDER-IDP-ERR-TNT-500",
+        Description: "No HTTP context available to retrieve tenant information. See https://bit.ly/errors-reference for more details."
+    );
+
+    public static readonly Error TenantDoesNotExist = new(
+        Code: "#VINDER-IDP-ERR-TNT-404",
+        Description: "The specified tenant does not exist. See https://bit.ly/errors-reference for more details."
+    );
+
+    public static readonly Error TenantHeaderMissing = new(
+        Code: "#VINDER-IDP-ERR-TNT-400",
+        Description: "Tenant header is missing from the HTTP request. See https://bit.ly/errors-reference for more details."
+    );
+
+    public static readonly Error TenantAlreadyExists = new(
+        Code: "#VINDER-IDP-ERR-TNT-409",
+        Description: "A tenant with the same name already exists. See https://bit.ly/errors-reference for more details."
+    );
+}
