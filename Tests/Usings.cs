684547af2d4c3e72fa81888072ed317ae0adc768
Richard Garcia <code.richardy@gmail.com>
2025-08-26 00:07:09 -0300

    this commit introduces global usings for sdk namespaces in the test suite

--- Tests/Usings.cs
+++ Tests/Usings.cs
@@ -2,6 +2,13 @@
 
 global using System.Security.Cryptography;
 
+/* global usings for SDK namespaces here */
+
+global using Vinder.IdentityProvider.Sdk.Clients;
+global using Vinder.IdentityProvider.Sdk.Extensions;
+global using Vinder.IdentityProvider.Sdk.Payloads.Identity;
+global using Vinder.IdentityProvider.Sdk.TestSuite.Fixtures;
+
 /* global usings for third-party namespaces here */
 
 global using DotNet.Testcontainers.Builders;
