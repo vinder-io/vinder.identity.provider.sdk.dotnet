4797fe879e2f6da45ca34cef1d687a515b99674c
Richard Garcia <code.richardy@gmail.com>
2025-08-26 21:44:25 -0300

    this commit introduces new global usings for common payloads and helpers

--- Source/Usings.cs
+++ Source/Usings.cs
@@ -1,8 +1,10 @@
 /* global usings for System namespaces here */
 
+global using System.Text;
 global using System.Text.Json;
 global using System.Net.Http.Headers;
 global using System.Net.Http.Json;
+global using System.Reflection;
 
 /* global usings for SDK namespaces here */
 
@@ -10,8 +12,11 @@ global using Vinder.IdentityProvider.Sdk.Contracts;
 global using Vinder.IdentityProvider.Sdk.Serializers;
 global using Vinder.IdentityProvider.Sdk.Errors;
 global using Vinder.IdentityProvider.Sdk.Extensions;
+global using Vinder.IdentityProvider.Sdk.Helpers;
 global using Vinder.IdentityProvider.Sdk.Configurations;
 
 global using Vinder.IdentityProvider.Sdk.Payloads.Identity;
 global using Vinder.IdentityProvider.Sdk.Payloads.Group;
 global using Vinder.IdentityProvider.Sdk.Payloads.Permission;
+global using Vinder.IdentityProvider.Sdk.Payloads.Common;
+global using Vinder.IdentityProvider.Sdk.Payloads.Tenant;
