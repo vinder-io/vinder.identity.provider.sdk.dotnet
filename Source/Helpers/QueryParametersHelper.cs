6f46498897844ef83d6b9d23ef87fc6c66495408
Richard Garcia <code.richardy@gmail.com>
2025-08-26 21:43:19 -0300

    this commit introduces the query parameters parser helper

---
+++ Source/Helpers/QueryParametersHelper.cs
@@ -0,0 +1,49 @@
+namespace Vinder.IdentityProvider.Sdk.Helpers;
+
+public static class QueryParametersParser
+{
+    public static string ToQueryString<TParameters>(TParameters instance)
+    {
+        if (instance is null) return string.Empty;
+
+        var properties = typeof(TParameters).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        var stringBuilder = new StringBuilder();
+
+        bool first = true;
+        foreach (var property in properties)
+        {
+            var value = property.GetValue(instance);
+            if (value is null) continue;
+
+            string name = ToCamelCase(property.Name);
+            string stringValue = value switch
+            {
+                bool builder => builder.ToString().ToLowerInvariant(),
+                _ => value?.ToString() ?? string.Empty
+            };
+
+            stringValue = Uri.EscapeDataString(stringValue);
+
+            if (!first)
+            {
+                stringBuilder.Append('&');
+            }
+            else
+            {
+                first = false;
+            }
+
+            stringBuilder.Append($"{name}={stringValue}");
+        }
+
+        return stringBuilder.ToString();
+    }
+
+    private static string ToCamelCase(string value)
+    {
+        if (string.IsNullOrEmpty(value) || char.IsLower(value[0]))
+            return value;
+
+        return char.ToLowerInvariant(value[0]) + value.Substring(1);
+    }
+}
