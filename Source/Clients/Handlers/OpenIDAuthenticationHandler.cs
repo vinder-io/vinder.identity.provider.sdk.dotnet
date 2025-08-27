bcbfcbb722250ea61a7e8105f85295760b751bd3
Richard Garcia <code.richardy@gmail.com>
2025-08-26 22:04:07 -0300

    this commit introduces the openid authentication handler

---
+++ Source/Clients/Handlers/OpenIDAuthenticationHandler.cs
@@ -0,0 +1,27 @@
+namespace Vinder.IdentityProvider.Sdk.Clients.Handlers;
+
+public class OpenIDAuthenticationHandler(IOpenIDConnectClient openIDClient, ClientCredentials credentials) : DelegatingHandler
+{
+    protected override async Task<HttpResponseMessage> SendAsync(
+        HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        var clientCredentials = new ClientAuthenticationCredentials
+        {
+            ClientId = credentials.ClientId,
+            ClientSecret = credentials.ClientSecret
+        };
+
+        var result = await openIDClient.AuthenticateAsync(clientCredentials, cancellationToken);
+        if (result.IsFailure || result.Data is null)
+        {
+            throw new InvalidOperationException($"{result.Error.Code} - {result.Error.Description}");
+        }
+
+        var token = result.Data.AccessToken;
+
+        request.Headers.Authorization =
+            new AuthenticationHeaderValue("Bearer", token);
+
+        return await base.SendAsync(request, cancellationToken);
+    }
+}
