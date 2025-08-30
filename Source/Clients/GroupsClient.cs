a0c9ecdcbe7d25f5e94a71d110b2a7ca10ebf329
Richard Garcia <code.richardy@gmail.com>
2025-08-30 18:56:48 -0300

    this commit introduces revoke and get group permissions features

--- Source/Clients/GroupsClient.cs
+++ Source/Clients/GroupsClient.cs
@@ -114,4 +114,59 @@ public sealed class GroupsClient(HttpClient httpClient) : IGroupsClient
             ? Result<GroupDetails>.Success(result)
             : Result<GroupDetails>.Failure(SdkErrors.DeserializationFailure);
     }
+
+    public async Task<Result> RevokeGroupPermissionAsync(Guid groupId, Guid permissionId, CancellationToken cancellation = default)
+    {
+        var response = await httpClient.DeleteAsync($"api/v1/groups/{groupId}/permissions/{permissionId}", cancellation);
+        if (response.StatusCode == System.Net.HttpStatusCode.Forbidden)
+        {
+            return Result.Failure(SdkErrors.Unauthorized);
+        }
+
+        if (response.IsSuccessStatusCode is false)
+        {
+            var error = await response.Content.ReadFromJsonAsync<Error>(
+                options: JsonSerialization.SerializerOptions,
+                cancellationToken: cancellation
+            );
+
+            return error is not null
+                ? Result.Failure(error)
+                : Result.Failure(SdkErrors.DeserializationFailure);
+        }
+
+        return Result.Success();
+    }
+
+    public async Task<Result<IReadOnlyCollection<PermissionDetails>>> GetGroupPermissionsAsync(Guid groupId, ListGroupPermissionsParameters parameters, CancellationToken cancellation = default)
+    {
+        var queryString = QueryParametersParser.ToQueryString(parameters);
+        var response = await httpClient.GetAsync($"api/v1/groups/{groupId}/permissions?{queryString}", cancellation);
+
+        if (response.StatusCode == System.Net.HttpStatusCode.Forbidden)
+        {
+            return Result<IReadOnlyCollection<PermissionDetails>>.Failure(SdkErrors.Unauthorized);
+        }
+
+        if (response.IsSuccessStatusCode is false)
+        {
+            var error = await response.Content.ReadFromJsonAsync<Error>(
+                options: JsonSerialization.SerializerOptions,
+                cancellationToken: cancellation
+            );
+
+            return error is not null
+                ? Result<IReadOnlyCollection<PermissionDetails>>.Failure(error)
+                : Result<IReadOnlyCollection<PermissionDetails>>.Failure(SdkErrors.DeserializationFailure);
+        }
+
+        var result = await response.Content.ReadFromJsonAsync<IReadOnlyCollection<PermissionDetails>>(
+            options: JsonSerialization.SerializerOptions,
+            cancellationToken: cancellation
+        );
+
+        return result is not null
+            ? Result<IReadOnlyCollection<PermissionDetails>>.Success(result)
+            : Result<IReadOnlyCollection<PermissionDetails>>.Failure(SdkErrors.DeserializationFailure);
+    }
 }
