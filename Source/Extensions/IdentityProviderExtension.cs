7fd65787a49b784b1481e2dc4c02d7e658559e2c
Richard Garcia <code.richardy@gmail.com>
2025-08-31 12:59:33 -0300

    this commit introduces AddIdentityProvider extension to register all SDK clients with BaseUrl, TenantHandler, and OpenIDAuthenticationHandler in a fluent way

---
+++ Source/Extensions/IdentityProviderExtension.cs
@@ -0,0 +1,39 @@
+namespace Vinder.IdentityProvider.Sdk.Extensions;
+
+public static class IdentityProviderExtension
+{
+    public static void AddIdentityProvider(this IServiceCollection services, Action<IdentityProviderOptions> configure)
+    {
+        var options = new IdentityProviderOptions();
+        configure(options);
+
+        services.AddSingleton<OpenIDAuthenticationHandler>();
+        services.AddSingleton<TenantHandler>();
+
+        services.AddSingleton(options);
+        services.AddHttpClient<IOpenIDConnectClient, OpenIDConnectClient>(client =>
+        {
+            client.BaseAddress = new Uri(options.BaseUrl);
+        });
+
+        services.AddSdkHttpClient<IIdentityClient, IdentityClient>(options.BaseUrl)
+            .WithTenant()
+            .WithAuthentication();
+
+        services.AddSdkHttpClient<IPermissionsClient, PermissionsClient>(options.BaseUrl)
+            .WithTenant()
+            .WithAuthentication();
+
+        services.AddSdkHttpClient<IGroupsClient, GroupsClient>(options.BaseUrl)
+            .WithTenant()
+            .WithAuthentication();
+
+        services.AddSdkHttpClient<ITenantsClient, TenantsClient>(options.BaseUrl)
+            .WithTenant()
+            .WithAuthentication();
+
+        services.AddSdkHttpClient<IUsersClient, UsersClient>(options.BaseUrl)
+            .WithTenant()
+            .WithAuthentication();
+    }
+}
