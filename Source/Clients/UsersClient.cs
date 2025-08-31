25aff8389ad8b60451176610e401d822fbee85fb
Richard Garcia <code.richardy@gmail.com>
2025-08-30 22:32:06 -0300

    this commit introduces the implementation for getting user groups and deleting a user.

--- Source/Clients/UsersClient.cs
+++ Source/Clients/UsersClient.cs
@@ -69,4 +69,61 @@ public sealed class UsersClient(HttpClient httpClient) : IUsersClient
             ? Result<IReadOnlyCollection<PermissionDetails>>.Success(result)
             : Result<IReadOnlyCollection<PermissionDetails>>.Failure(SdkErrors.DeserializationFailure);
     }
+
+    public async Task<Result<IReadOnlyCollection<GroupBasicDetails>>> GetUserGroupsAsync(
+        Guid userId, ListUserAssignedGroupsParameters? parameters = null, CancellationToken cancellation = default)
+    {
+        string queryString = QueryParametersParser.ToQueryString(parameters);
+        string url = $"api/v1/users/{userId}/groups?{queryString}";
+
+        var response = await httpClient.GetAsync(url, cancellation);
+        if (response.StatusCode == System.Net.HttpStatusCode.Forbidden)
+        {
+            return Result<IReadOnlyCollection<GroupBasicDetails>>.Failure(SdkErrors.Unauthorized);
+        }
+
+        if (response.IsSuccessStatusCode is false)
+        {
+            var error = await response.Content.ReadFromJsonAsync<Error>(
+                options: JsonSerialization.SerializerOptions,
+                cancellationToken: cancellation
+            );
+
+            return error is not null
+                ? Result<IReadOnlyCollection<GroupBasicDetails>>.Failure(error)
+                : Result<IReadOnlyCollection<GroupBasicDetails>>.Failure(SdkErrors.DeserializationFailure);
+        }
+
+        var result = await response.Content.ReadFromJsonAsync<IReadOnlyCollection<GroupBasicDetails>>(
+            options: JsonSerialization.SerializerOptions,
+            cancellationToken: cancellation
+        );
+
+        return result is not null
+            ? Result<IReadOnlyCollection<GroupBasicDetails>>.Success(result)
+            : Result<IReadOnlyCollection<GroupBasicDetails>>.Failure(SdkErrors.DeserializationFailure);
+    }
+
+    public async Task<Result> DeleteUserAsync(Guid userId, CancellationToken cancellation = default)
+    {
+        var response = await httpClient.DeleteAsync($"api/v1/users/{userId}", cancellation);
+        if (response.StatusCode == System.Net.HttpStatusCode.Forbidden)
+        {
+            return Result.Failure(SdkErrors.Unauthorized);
+        }
+
+        if (response.IsSuccessStatusCode is false)
+        {
+            var error = await response.Content.ReadFromJsonAsync<Error>(
+                options: JsonSerialization.SerializerOptions,
+                cancellationToken: cancellation
+            );
+
+            return error is not null
+                ? Result.Failure(error)
+                : Result.Failure(SdkErrors.DeserializationFailure);
+        }
+
+        return Result.Success();
+    }
 }
