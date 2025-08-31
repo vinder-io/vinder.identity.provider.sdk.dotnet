ae999c3b3322435e6bd03ee668295d4a8a107359
Richard Garcia <code.richardy@gmail.com>
2025-08-31 12:58:08 -0300

    this commit introduces TenantHandler to automatically add the x-tenant header to outgoing requests

---
+++ Source/Clients/Handlers/TenantHandler.cs
@@ -0,0 +1,13 @@
+namespace Vinder.IdentityProvider.Sdk.Clients.Handlers;
+
+public sealed class TenantHandler(IdentityProviderOptions options) : DelegatingHandler
+{
+    protected override async Task<HttpResponseMessage> SendAsync(
+        HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        request.Headers.Remove("x-tenant");
+        request.Headers.Add("x-tenant", options.Tenant);
+
+        return await base.SendAsync(request, cancellationToken);
+    }
+}
