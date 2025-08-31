84e518cb22d43a6fc65f638a62dff103db72f4b5
Richard Garcia <code.richardy@gmail.com>
2025-08-31 12:59:07 -0300

    this commit introduces HttpClientBuilderExtensions to fluently register SDK HttpClients with TenantHandler and OpenIDAuthenticationHandler

---
+++ Source/Extensions/HttpClientBuilderExtension.cs
@@ -0,0 +1,24 @@
+namespace Vinder.IdentityProvider.Sdk.Extensions;
+
+public static class HttpClientBuilderExtensions
+{
+    public static IHttpClientBuilder AddSdkHttpClient<TClient, TImplementation>(this IServiceCollection services, string baseUrl)
+        where TClient : class
+        where TImplementation : class, TClient
+    {
+        return services.AddHttpClient<TClient, TImplementation>(client =>
+        {
+            client.BaseAddress = new Uri(baseUrl);
+        });
+    }
+
+    public static IHttpClientBuilder WithTenant(this IHttpClientBuilder builder)
+    {
+        return builder.AddHttpMessageHandler<TenantHandler>();
+    }
+
+    public static IHttpClientBuilder WithAuthentication(this IHttpClientBuilder builder)
+    {
+        return builder.AddHttpMessageHandler<OpenIDAuthenticationHandler>();
+    }
+}
