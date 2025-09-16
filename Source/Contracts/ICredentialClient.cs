624d4dff6ea92707a01d1afd5acda439db995289
Richard Garcia <code.richardy@gmail.com>
2025-09-16 20:00:11 -0300

    this commit introduces the removal of the contract files to simplify the project structure and reduce the number of interfaces, making the SDK easier to maintain and understand

--- Source/Contracts/ICredentialClient.cs
+++
@@ -1,9 +0,0 @@
-namespace Vinder.IdentityProvider.Sdk.Contracts;
-
-public interface IOpenIDConnectClient
-{
-    public Task<Result<ClientAuthenticationResult>> AuthenticateAsync(
-        ClientAuthenticationCredentials credentials,
-        CancellationToken cancellation = default
-    );
-}
