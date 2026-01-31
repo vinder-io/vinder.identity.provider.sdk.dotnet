c183d41ff4ff37d4549fc892d1f1a9aa7dc54626
Richard Garcia <code.richardy@gmail.com>
2026-01-31 10:57:18 -0300

    refactor: clean up global usings and correct namespace reference

--- Tests/Usings.cs
+++ Tests/Usings.cs
@@ -1,21 +1,14 @@
-/* global usings for System namespaces here */
-
 global using System.Security.Cryptography;
-
-/* global usings for SDK namespaces here */
-
 global using Vinder.Federation.Sdk.Clients;
-global using Vinder.Federation.Sdk.Errors;
 
 global using Vinder.Federation.Sdk.Extensions;
+global using Vinder.Federation.Sdk.Contracts.Errors;
 global using Vinder.Federation.Sdk.Contracts.Payloads.Identity;
 global using Vinder.Federation.Sdk.Contracts.Payloads.Group;
 global using Vinder.Federation.Sdk.Contracts.Payloads.Permission;
 global using Vinder.Federation.Sdk.Contracts.Payloads.Tenant;
 global using Vinder.Federation.Sdk.Contracts.Payloads.User;
 global using Vinder.Federation.Sdk.TestSuite.Fixtures;
-
-/* global usings for third-party namespaces here */
 
 global using DotNet.Testcontainers.Builders;
 global using DotNet.Testcontainers.Containers;
